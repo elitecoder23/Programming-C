bd393a8edd18260071e28a51f724defba010ec38
Anish Nag <anishnag01@gmail.com>
2026-06-14 20:49:32 -0500

    Practiced with different data types in C

---
+++ DataTypes.c
@@ -0,0 +1,23 @@
+// File to practice writing different data types 
+#include <stdio.h>
+
+
+int main(){
+
+    // integer
+    int numInteger = 5;
+
+    // float
+    float numFloat = 5.0;
+
+    // double
+    double numDouble = 5.12345;
+
+    // character
+    char letterA = 'A';
+
+    printf("Integer Number: %d\n", numInteger);
+    printf("Float number: %.1f\n", numFloat);
+    printf("Double number: %.5lf\n", numDouble);
+    printf("Grade: %c\n", letterA);
+}
